939f4a4bc07c9cf1f217dc4e0376fcaf45122a1c
S317 <seifenan@gmail.com>
2022-02-18 03:53:33 -0500

    modifying router; testing deployment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.css';
 import Particles from 'react-tsparticles';
@@ -20,12 +20,14 @@ function App() {
             <Particles options={particlesOptions} />
             <Router>
                 <NavBar />
-                <Route exact path="/" component={Main} />
-                <Route path="/about" component={About} />
-                <Route path="/featured" component={Featured} />
-                <Route path="/portfolio" component={Projects} />
-                <Route path="/resume" component={Resume} />
-                <Route path="/contact" component={Contact} />
+                <Switch>
+                    <Route exact path="/" component={Main} />
+                    <Route path="/about" component={About} />
+                    <Route path="/featured" component={Featured} />
+                    <Route path="/portfolio" component={Projects} />
+                    <Route path="/resume" component={Resume} />
+                    <Route path="/contact" component={Contact} />
+                </Switch>
                 <Footer />
             </Router>
         </div>
